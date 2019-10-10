cf2f1aad9d64dde002d48a12feaca0e170065a49
Iliess67 <el-moumene@hotmail.com>
2019-10-10 00:28:08 -0400

    End of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component, Fragment } from 'react';
+import NavBar from '../../features/nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
     return (
-      <div>
-        <h1>Re-vents</h1>
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard/>
+        </Container>
+      </Fragment>
     );
   }
 }
